2d76ebe6bcd0548eca471f4ef6014673de64c973
DanielHun20 <papp.daniel20@gmail.com>
2021-04-21 11:26:42 +0200

    UrlapValidacio 2.0

--- public_html/urlapkezeles.js
+++ public_html/urlapkezeles.js
@@ -37,6 +37,31 @@ function validalas(){
         
         
         
+        var tel = /[0-9]{2} [0-9]{3} [0-9]{4}/;
+    if (!phone.test(ID("nev").value)) {
+        uzenet += " + jellel kezdődjön és legyen köztük kötőjel! <br>";
+        ID("phone").style.border = "2px solid red";
+    } else {
+        ID("phone").style.border = "none";
+        megadottAdatok += "Telefon: " + ID("phone").value + "<br>";
+
+    }
+        
+        var email = /b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/b;
+    if (!email.test(ID("email").value)) {
+        uzenet += "Kötelező @ jel használata és . jel után kettő karakter szerepelhet <br>";
+        ID("email").style.border="2px solid red";
+    }
+    else{
+        ID("email").style.border="none";
+        magadottAdatok += "E-mail: "+ ID("email").value+"<br>";
+    }
+        
+        
+        
+        
+        
+        
    //  console.log(uzenet);
        $("aside section:nth-child(1) p")[0].innerHTML=uzenet;
        $("aside section:nth-child(2) p")[0].innerHTML=megadottAdatok;
